6175d2120d79c1c23cac679c68547cbddece088e
Kevin Batdorf <kbat82@gmail.com>
2019-07-23 19:53:54 +0700

    remove some useless code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,15 @@
 <template>
 	<div class="p-4">
-		<button id="basic-noti">home</button>
+		home
 	</div>
 </template>
 
 <script>
-const { ipcRenderer } = require('electron')
 export default {
 	data() {
-		return {
-			message: 'From home.'
-		}
+		return {}
 	},
 	mounted() {
-		const notification = {
-			title: 'Basic Notification',
-			body: 'Short message part'
-		}
-
-		const notificationButton = document.getElementById('basic-noti')
-
-		notificationButton.addEventListener('click', () => {
-			const myNotification = new window.Notification(notification.title, notification)
-
-			myNotification.onclick = () => {
-				console.log('Notification clicked')
-			}
-
-			console.log(ipcRenderer.sendSync('set-setting', {option: 'apiToken', value: 'hi'}))
-
-		})
 	}
 }
 </script>
